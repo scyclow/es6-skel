f1d558999123d740d19398dae237ac0d7c7fada3
scyclow <spike716@gmail.com>
2015-12-24 22:25:25 -0600

    added bourbon bitters; fixed package.json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Provider } from 'react-redux';
 
-require('../styles/reset.scss');
+require('../styles/application.scss');
 
 ReactDOM.render(
   // <Provider store={store}>
